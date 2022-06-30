2e65d836e8a7a819b70307b689e408d884627443
Alec Keeler <alec.keeler@gmail.com>
2022-06-30 15:09:37 -0500

    complete single boardgame page

--- backend/routes/api/boardgames.js
+++ backend/routes/api/boardgames.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { Boardgame } = require('../../db/models');
+const { Boardgame, Review, sequelize } = require('../../db/models');
 
 router.get('/', async(req, res) => {
     const games = await Boardgame.findAll({
@@ -10,6 +10,38 @@ router.get('/', async(req, res) => {
     })
 
     res.json({games})
+})
+
+router.get('/:id', async(req, res) => {
+    const game = await Boardgame.findByPk(req.params.id, {
+        include: {model: Review, attributes: ['id', 'content']},
+    })
+
+    const numReviews = await Review.count({
+        where: {gameId: game.id}
+    })
+
+    const data = {}
+    data.game = {
+        id: game.id, 
+        name: game.name, 
+        maxPlayers: game.maxPlayers, 
+        avgRating: game.avgRating,
+        category: game.category,
+        createdAt: game.createdAt, 
+        updatedAt: game.updatedAt
+    }
+    data.numReviews = numReviews
+
+    data.reviews = game.Reviews
+
+    if (!game) {
+        const err = new Error('The specified boardgame does not exist')
+        err.status = 404
+        res.json(err)
+    } else {
+        res.json(data)
+    }
 })
 
 
